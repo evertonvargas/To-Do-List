fc58fc65762cf4983560d184a03f671e7e8a755c
evertonvargas <evertonpassa7@gmail.com>
2021-06-10 18:44:31 -0300

    renderizando items do database no app

--- app.js
+++ app.js
@@ -1,31 +1,72 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const date = require(__dirname + "/data.js");
 const port = 3000;
 let items = [];
+let workItems = [];
 
 const app = express();
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
-app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+function insert() {
+  const item1 = new Item({ name: "Bem-vindo ao to-do-list" });
+  const item2 = new Item({ name: "Add um novo item" });
+  const defaultItems = [item1, item2];
+
+  Item.insertMany(defaultItems, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Cadastro realizado com sucesso");
+    }
+  });
+}
+
 app.get("/", (req, res) => {
-  let today = new Date();
-
-  let options = {
-      weekday: "long",
-      day: "numeric",
-      month: "long"
-  }
-
-  let day = today.toLocaleDateString("pt-BR", options)
-
-  res.render("list", { kindofday: day, newListItem: items});
+  Item.find({}, (err, foundItems) => {
+    if (foundItems.length === 0) {
+      insert();
+      res.redirect("/")
+    } else {
+      res.render("list", {
+        listTitle: date.getDate(),
+        newListItem: foundItems,
+      });
+    }
+  });
 });
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
-    items.push(item)
-    res.redirect("/")
+  let item = req.body.newItem;
+  if (req.body.list === "Work list") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work list", newListItem: workItems });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
