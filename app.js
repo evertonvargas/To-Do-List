be243565f2e861a64a042bde53f0d5b3413fadec
evertonvargas <evertonpassa7@gmail.com>
2021-06-11 16:21:42 -0300

    colocando site no ar

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const date = require(__dirname + "/data.js");
 const port = 3000;
-let items = [];
-let workItems = [];
-
+const urlCluster = process.env.HOST;
 const app = express();
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect(urlCluster, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const itemsSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
+});
+
+const listSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  itemList: [itemsSchema]
 });
 
 const Item = mongoose.model("Item", itemsSchema);
+const List = mongoose.model("List", listSchema);
+
+const item1 = new Item({ name: "Bem-vindo ao to-do-list" });
+const item2 = new Item({ name: "Add um novo item" });
+const defaultItems = [item1, item2];
 
 function insert() {
-  const item1 = new Item({ name: "Bem-vindo ao to-do-list" });
-  const item2 = new Item({ name: "Add um novo item" });
-  const defaultItems = [item1, item2];
-
   Item.insertMany(defaultItems, function (err) {
     if (err) {
       console.log(err);
@@ -59,24 +66,20 @@ app.post("/", (req, res) => {
 });
 
 app.post("/delete", (req, res) => {
-  Item.findByIdAndRemove(req.body.checkbox, function(err){
-    if(!err){
-      console.log("Item deletado com sucesso")
+  Item.findByIdAndRemove(req.body.checkbox, function (err) {
+    if (!err) {
+      console.log("Item deletado com sucesso");
       res.redirect("/");
-    }else{
-      console.log(err)
+    } else {
+      console.log(err);
     }
-  })
-});
-
-app.get("/work", (req, res) => {
-  res.render("list", { listTitle: "Work list", newListItem: workItems });
+  });
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
